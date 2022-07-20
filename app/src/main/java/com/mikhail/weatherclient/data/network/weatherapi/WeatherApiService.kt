797f10b11522e8088f466b5cc4a5c28799f7aa48
sozidatel319 <mbogat@yandex.ru>
2022-07-20 03:14:13 +0300

    refactoring

--- app/src/main/java/com/mikhail/weatherclient/data/network/weatherapi/WeatherApiService.kt
+++ app/src/main/java/com/mikhail/weatherclient/data/network/weatherapi/WeatherApiService.kt
@@ -1,5 +1,6 @@
 package com.mikhail.weatherclient.data.network.weatherapi
 
+import com.mikhail.weatherclient.BuildConfig
 import com.mikhail.weatherclient.model.WeatherModel
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -10,13 +11,13 @@ interface WeatherApiService {
     suspend fun getWeather(
         @Query("q") q: String?,
         @Query("units") measure: String = "metric",
-        @Query("appid") key: String = "0507febdbdf6a636ec6bdcdfe0b909fc"
+        @Query("appid") key: String = BuildConfig.THE_WEATHER_DATABASE_API
     ): WeatherModel
 
     @GET("data/2.5/forecast")
     suspend fun getWeatherByCoords(
         @Query("lat") lat: Float?,
         @Query("lon") lon: Float?,
-        @Query("appid") key: String?
+        @Query("appid") key: String = BuildConfig.THE_WEATHER_DATABASE_API
     ): WeatherModel
 }
